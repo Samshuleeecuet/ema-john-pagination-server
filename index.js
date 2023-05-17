952488693c4ff949847b677e473f658e65a9675d
Samshuleeecuet <samshuleeeweb@gmail.com>
2023-05-17 21:48:47 +0600

    complete

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.df1ioxo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,9 +34,28 @@ async function run() {
     const productsCollection = client.db('ema-john-collection').collection('products')
     
     app.get('/products',async(req,res)=>{
-        const products = await productsCollection.find().toArray()
+      console.log(req.query)
+      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = page*limit;
+        const products = await productsCollection.find().skip(skip).limit(limit).toArray()
         res.send(products);
     })
+
+    app.get('/totalProducts',async(req,res)=>{
+      const totalProducts = await productsCollection.estimatedDocumentCount();
+      res.send({totalProducts: totalProducts})
+    })
+
+    app.post('/productByIds',async(req,res)=>{
+      const ids = req.body;
+      console.log(ids)
+      const objectIds = ids.map(id=> new ObjectId(id))
+      const query = {_id: {$in: objectIds}}
+      const result = await productsCollection.find(query).toArray();
+      res.send(result)
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
